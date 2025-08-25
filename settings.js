c588fe9363d0faf5020bb476bb5db8d2727973d9
UltraLegeend <135343923+UltraLegeend@users.noreply.github.com>
2025-08-25 17:56:34 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "VINCE MD",
   botOwner: 'Vinnie', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254769125714', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
