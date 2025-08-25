f92808ed1a76de3acc1ed563bb840affeafc507d
UltraLegeend <135343923+UltraLegeend@users.noreply.github.com>
2025-08-25 17:54:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const store = {
 let phoneNumber = "911234567890"
 let owner = JSON.parse(fs.readFileSync('./data/owner.json'))
 
-global.botname = "KNIGHT BOT"
+global.botname = "VINCE MD"
 global.themeemoji = "•"
 
 const settings = require('./settings')
@@ -149,13 +149,13 @@ async function startXeonBotInc() {
                 // Only try to send error message if we have a valid chatId
                 if (mek.key && mek.key.remoteJid) {
                     await XeonBotInc.sendMessage(mek.key.remoteJid, { 
-                        text: '❌ An error occurred while processing your message.',
+                        text: '❌ An error occurred while processing your message🥲.',
                         contextInfo: {
                             forwardingScore: 1,
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
                                 newsletterJid: '120363161513685998@newsletter',
-                                newsletterName: 'KnightBot MD',
+                                newsletterName: 'Vince MD',
                                 serverMessageId: -1
                             }
                         }
@@ -249,7 +249,7 @@ async function startXeonBotInc() {
             const botNumber = XeonBotInc.user.id.split(':')[0] + '@s.whatsapp.net';
             await XeonBotInc.sendMessage(botNumber, { 
                 text: `🤖 Bot Connected Successfully!\n\n⏰ Time: ${new Date().toLocaleString()}\n✅ Status: Online and Ready!
-                \n✅Make sure to join below channel`,
+                \nEnjoy🎉`,
                 contextInfo: {
                     forwardingScore: 1,
                     isForwarded: true,
